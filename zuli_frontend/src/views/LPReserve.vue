ebd3dce989aa845caeed2842b947ee86f4e33bfe
Geiner <geijomontoya@gmail.com>
2026-04-19 12:50:09 -0600

    Creacion de barra de navegacion landing page

---
+++ zuli_frontend/src/views/LPReserve.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div class = "landing-page">
+        <TopNavBar />
+        <h1>Estamos cooking</h1>
+        <p></p>
+    </div>
+</template>
+
+<script>
+import TopNavBar from '../components/TopNavBar.vue';
+    export default {
+        components: {
+            TopNavBar
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
