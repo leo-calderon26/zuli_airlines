e87b9a2cd054c0390c73da56b2ef35c1c1098112
Geiner <geijomontoya@gmail.com>
2026-04-19 13:38:55 -0600

    Lnading page con routing funcional y BottomBar

--- zuli_frontend/router/index.js
+++ zuli_frontend/router/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../src/views/Login.vue";
 
@@ -19,3 +20,24 @@ const router = createRouter({
 });
 
 export default router;
+=======
+import { createRouter, createWebHistory } from 'vue-router'
+import LPReserve from '../src/views/LPReserve.vue'
+import LPCheckIn from '../src/views/LPCheckIn.vue'
+import LPConsult from '../src/views/LPConsult.vue'
+import LPHelp from '../src/views/LPHelp.vue'
+
+const routes = [
+  { path: '/', name: 'reserve', component: LPReserve },
+  { path: '/Check-in', name: 'checkin', component: LPCheckIn },
+  { path: '/Consulta', name: 'consult', component: LPConsult },
+  { path: '/Ayuda', name: 'help', component: LPHelp }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
+>>>>>>> 998718c (Lnading page con routing funcional y BottomBar)
