f2d6ba34279a20afa724f82aa49dc50c335cc7b7
Geiner <geijomontoya@gmail.com>
2026-04-19 13:38:55 -0600

    Lnading page con routing funcional y BottomBar

---
+++ zuli_frontend/src/components/PublicBottomBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="public-bottom-bar">
+    <div class="bottom-content">
+      <div>xxxxxxxxxx</div>
+      <div>xxxxxxxxxx</div>
+      <div>xxxxxxxxxx</div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.public-bottom-bar {
+  width: 100%;
+  background-color: #711717;
+  color: white;
+  padding: 18px 24px;
+  box-sizing: border-box;
+}
+
+.bottom-content {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 20px;
+  font-weight: 600;
+}
+
+@media (max-width: 768px) {
+  .bottom-content {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+}
+</style>
