849c6d55ea195575fb5fe862596d2a5d374d8baa
randy rojas p <randy.rojas@ucr.ac.cr>
2026-04-19 23:22:09 -0600

    Creacion de archivo de exepciones para ser usado por el middleware con el metodo NotfoundExeption

---
+++ zuli_backend/zuli_Data/Exceptions/AppExeptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace zuli_Data.Exceptions
+{
+    // Esta clase esta hecha para que la use el middleware
+    public abstract class AppExeptions : Exception
+    {
+        public int StatusCode { get; }
+        public string ErrorCode { get; }
+
+        protected AppExeptions(string message, int statusCode, string errorCode) : base(message) {
+            StatusCode = statusCode;
+            ErrorCode = errorCode;
+        }
+    }
+
+    public class NotFoundException : AppExeptions
+    {
+        public NotFoundException(string msg) 
+        : base(msg, StatusCodes.Status404NotFound,"NOT_FOUND") { }
+    }
+}
