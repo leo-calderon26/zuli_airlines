d5439f8faad933ffd28bf4e5fdb1fd2523e58659
randy rojas p <randy.rojas@ucr.ac.cr>
2026-04-19 23:25:47 -0600

    Se agrego el middleware al backend ademas de la conexion con la base de datos y se creo una clase MiscValidor que es el encargado de validar se los datos ingresado por el usario son correctos mediante expresiones regulares y funciones de C#

--- zuli_backend/zuli_backend/Program.cs
+++ zuli_backend/zuli_backend/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using zuli_backend.Middleware;
+using zuli_Buisiness;
+using zuli_Buisiness.Interface;
+using zuli_Data;
+using zuli_Repository;
+using zuli_Repository.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +15,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Conexión a la base de datos usando SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddScoped<IAircraftService, AircraftService>();
+builder.Services.AddScoped<IAircraftRepository, AircraftRepository>();
+
 var app = builder.Build();
+app.UseGlobalExeption();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ zuli_backend/zuli_buisiness/Validation/MiscValidor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace zuli_Buisiness.Validation
+{
+    public static class MiscValidor
+    {
+        public static bool ContainsNumber(string input) 
+        {
+            return !string.IsNullOrWhiteSpace(input) && Regex.IsMatch(input, @"\d");
+        }
+    }
+}
