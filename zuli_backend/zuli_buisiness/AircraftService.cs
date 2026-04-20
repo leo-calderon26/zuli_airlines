100f37fdeb38203d8835245afacc5a2aa5533b7d
randy rojas p <randy.rojas@ucr.ac.cr>
2026-04-19 23:11:18 -0600

    Implementacion de una parte de la logica para pasar el DTO a una entity para posteriormente ser insertado en la capa repository

---
+++ zuli_backend/zuli_buisiness/AircraftService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using zuli_Buisiness.DTO;
+using zuli_Buisiness.Interface;
+using zuli_Data.Entities;
+using zuli_Repository.Interface;
+using zuli_Data.Entities;
+using zuli_Data.Exceptions;
+
+namespace zuli_Buisiness
+{
+    public class AircraftService : IAircraftService
+    {
+        // Inyeccion de dependencias
+        private readonly IAircraftRepository _repository;
+        public AircraftService(IAircraftRepository repository) => _repository = repository;
+        
+        public async Task<AircraftDTO?> CreateAircraft(AircraftDTO aircraft) 
+        {
+            // TODO(randy) arreglar esto
+            if(aircraft == null) throw new ArgumentNullException(nameof(aircraft));
+
+            var newAircraft = new AircraftEntity
+            {
+                AircraftId = aircraft.AircraftId,
+                numberEconomyClassRows = aircraft.numberEconomyClassRows,
+                numberSeatingRowsEconomy = aircraft.numberSeatingRowsEconomy,
+                numberFirstClassRows = aircraft.numberFirstClassRows,
+                numberSeatingRowsFirst = aircraft.numberSeatingRowsFirst,
+                model = aircraft.model,
+                weight = aircraft.weight
+
+
+            };
+
+            var aircraftCreate = await _repository.CreateAircraft(newAircraft);
+            // TODO(randy) Cambiar esta manejo de error
+
+            if(aircraftCreate is null)
+            {
+                throw new NotFoundException($"The aircraft with the id {newAircraft.AircraftId} cannot be created");
+            }
+
+            return new AircraftDTO
+            {
+                AircraftId = aircraft.AircraftId,
+                numberEconomyClassRows = aircraft.numberEconomyClassRows,
+                numberSeatingRowsEconomy = aircraft.numberSeatingRowsEconomy,
+                numberFirstClassRows = aircraft.numberFirstClassRows,
+                numberSeatingRowsFirst = aircraft.numberSeatingRowsFirst,
+                model = aircraft.model,
+                weight = aircraft.weight
+
+            };
+        }
+    }
+}
---
+++ zuli_backend/zuli_buisiness/Interface/IAircraftService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using zuli_Buisiness.DTO;
+
+namespace zuli_Buisiness.Interface
+{
+    public interface IAircraftService
+    {
+        Task<AircraftDTO?> CreateAircraft(AircraftDTO aircraft);
+    }
+}
