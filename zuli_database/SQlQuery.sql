b1f0002b0d221a00959c69ead8cc65e8fd08aeab
randy rojas p <randy.rojas@ucr.ac.cr>
2026-04-22 13:17:25 -0600

    Cambios en la estructura de la base datos en un formato

--- zuli_database/SQlQuery.sql
+++ zuli_database/SQlQuery.sql
@@ -1,14 +1,11 @@
-
 Create Database ZuliAzirlines
 use ZuliAzirlines
-Drop database Zuli
 CREATE TABLE Aircraft (
-  idAircraft int PRIMARY KEY NOT NULL,
+  aircraftId int PRIMARY KEY NOT NULL,
   numberEconomyClassRows smallint check(numberEconomyClassRows >= 0),
   numberSeatingRowsEconomy smallint check(numberSeatingRowsEconomy >= 0),
   numberFirstClassRows smallint check(numberFirstClassRows >= 0),
-  numberSeatingFirstRows smallint check(numberSeatingFirstRows >= 0),
+  numberSeatingRowsFirst smallint check(numberSeatingRowsFirst >= 0),
   model varchar(15) check (model NOT LIKE '% %'),
-  weight decimal(10, 2) check (weight < 1000) 
+  weight decimal(10, 2)
 );
- 
