9163f54bf13641f0dc19bc04f0821c7235ee3046
randy rojas p <randy.rojas@ucr.ac.cr>
2026-04-22 18:45:26 -0600

    Cambios en la estructuras de la tabla, ya que ahora no es necesario ingresar el Id

--- zuli_backend/zuli_Data/Entites/AircraftEntity.cs
+++ zuli_backend/zuli_Data/Entites/AircraftEntity.cs
@@ -7,12 +7,12 @@ namespace zuli_Data.Entities
 {
     public class AircraftEntity
     {
-        [Key] public int AircraftId {  get; set; }
+        public Guid aircraftId { get; set; }
+        public string model {  get; set; }
+        public decimal weight { get; set; }
         public short numberEconomyClassRows { get; set; }
         public short numberSeatingRowsEconomy { get; set; }
         public short numberFirstClassRows { get; set; }
         public short numberSeatingRowsFirst { get; set; }
-        public string model {  get; set; }
-        public decimal weight { get; set; }
     }
 }
--- zuli_backend/zuli_buisiness/DTO/AircraftDTO.cs
+++ zuli_backend/zuli_buisiness/DTO/AircraftDTO.cs
@@ -6,12 +6,11 @@ namespace zuli_Buisiness.DTO
 {
     public class AircraftDTO
     {
-        public int aircraftId { get; set; }
+        public string model { get; set; }
+        public decimal weight { get; set; }
         public short numberEconomyClassRows { get; set; }
         public short numberSeatingRowsEconomy { get; set; }
         public short numberFirstClassRows { get; set; }
         public short numberSeatingRowsFirst { get; set; }
-        public string model { get; set; }
-        public decimal weight { get; set; }
     }
 }
