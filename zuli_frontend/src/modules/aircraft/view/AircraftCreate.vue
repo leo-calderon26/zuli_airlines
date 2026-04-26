4685a28b75b8c30c59cb259b5d5ba50559393d5c
randy rojas p <randy.rojas@ucr.ac.cr>
2026-04-26 00:18:22 -0600

    Formulario para crear una aeronave de usado v-model

--- zuli_frontend/src/modules/aircraft/view/AircraftCreate.vue
+++ zuli_frontend/src/modules/aircraft/view/AircraftCreate.vue
@@ -1,12 +1,13 @@
 <script setup>
 import HeaderComponent from '../../../shared/HeaderComponent.vue';
-
+import AircraftForm from '../components/AircraftForm.vue';
 
 </script>
 
 <template>
     <HeaderComponent/>    
     <h1>Aqui se va enseñar el form para crear una aeronave</h1>
+    <AircraftForm/>
 </template>
 
 <style>
