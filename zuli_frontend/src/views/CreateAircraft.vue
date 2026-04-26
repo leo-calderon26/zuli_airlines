e0a5cfe904c089048dcad9371f72e9aabf756a22
randy rojas p <randy.rojas@ucr.ac.cr>
2026-04-25 20:27:03 -0600

    Se creo las view para ver las aeronave listadas

---
+++ zuli_frontend/src/views/CreateAircraft.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeaderComponent from '../components/HeaderComponent.vue';
+
+</script>
+
+<template>
+    <HeaderComponent></HeaderComponent>
+    <h1>Aqui se va enseñar el form para crear una aeronave</h1>
+</template>
+
+<style>
+</style>
