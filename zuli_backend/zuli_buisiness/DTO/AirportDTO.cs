c328fdc8064802d4658cd4e915412d30f8886783
Luis Arias <luisalfredo.arias@ucr.ac.cr>
2026-04-28 17:40:05 -0600

    Crear la Entity y el DTO del aeropuerto

---
+++ zuli_backend/zuli_buisiness/DTO/AirportDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zuli_Buisiness.DTO
+{
+    public class AirportDTO
+    {
+        public string airportCode { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public Guid adminId { get; set; }
+    }
+}
