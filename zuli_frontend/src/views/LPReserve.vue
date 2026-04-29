465acff44c0f4923c81477aa660f8fea5ab94412
Geiner <geijomontoya@gmail.com>
2026-04-29 13:39:43 -0600

    Cambio en selectores de informacion  en vista de LandingPage reserve

---
+++ zuli_frontend/src/views/LPReserve.vue
@@ -0,0 +1,41 @@
+
+<template>
+    <div class = "landing-page">
+        <PublicNavBar />
+        <main class="reserve-card-center">
+            <ReserveCard />
+        </main>
+        <PublicBottomBar />
+    </div>
+</template>
+
+<script>
+import PublicNavBar from '../components/PublicNavBar.vue';
+import PublicBottomBar from '../components/PublicBottomBar.vue';
+import ReserveCard from '../components/ReserveCard.vue';
+    export default {
+        components: {
+            PublicNavBar,
+            PublicBottomBar,
+            ReserveCard
+        }
+    }
+</script>
+
+<style scoped>
+.landing-page {
+  min-height: 100svh;
+  display: flex;
+  flex-direction: column;
+}
+
+.reserve-card-center {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem;
+    transform: translateY(-30px);
+}
+
+</style>
