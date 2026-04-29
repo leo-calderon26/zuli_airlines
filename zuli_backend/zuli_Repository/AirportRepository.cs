c431f5565a13cf458cc50cc77f62b5b49aaafdf4
Luis Arias <luisalfredo.arias@ucr.ac.cr>
2026-04-28 19:50:22 -0600

    Creacion de repositorio e interfaz de aeropuertos para insertar y validar si ya existe

---
+++ zuli_backend/zuli_Repository/AirportRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using zuli_Data;
+using zuli_Data.Entities;
+using zuli_Repository.Interface;
+using Dapper;
+
+namespace zuli_Repository
+{
+    public class AirportRepository : IAirportRepository
+    {
+        private readonly DapperContext _context;
+
+        public AirportRepository(DapperContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<int> CreateAirport(AirportEntity airport)
+        {
+            using var connection = _context.CreateConnection();
+
+            var insertSql = @"
+                        INSERT INTO Airport (AirportCode, Name, Country, City, AdminId)
+                        VALUES (@AirportCode, @Name, @Country, @City, @AdminId)";
+
+            return await connection.ExecuteScalarAsync<int>(insertSql, new
+            {
+                airportCode = airport.AirportCode,
+                name = airport.Name,
+                country = airport.Country,
+                city = airport.City,
+                adminId = airport.AdminId
+            });
+        }
+
+        /// <summary>
+        /// Verifica que un aeropuerto no exista en la base de datos
+        /// </summary>
+        /// <param name="airportCode">Codigo a comparar</param>
+        /// <returns>
+        /// <c>true</c> si el aeropuerto existe; en caso contrario, <c>false</c>
+        /// </returns>
+        public async Task<bool> AlreadyExist(string airportCode)
+        {
+            using var connection = _context.CreateConnection();
+            var sql = "SELECT COUNT(1) FROM Airport WHERE AirportCode = @airportCode";
+            var count = await connection.ExecuteScalarAsync<int>(
+                sql,
+                new { airportCode = airportCode }
+            );
+
+            return count > 0;
+        }
+
+    }
+}
