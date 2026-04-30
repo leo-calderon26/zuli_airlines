64ca72c9aa7c04d77742a3e35ea96247ab44c4a2
Geiner <geijomontoya@gmail.com>
2026-04-29 22:53:46 -0600

    Migracion a tailwind del landingPage

--- zuli_frontend/src/modules/landing/view/ReserveView.vue
+++ zuli_frontend/src/modules/landing/view/ReserveView.vue
@@ -1,10 +1,11 @@
 
 <template>
-    <div class="flex flex-col bg-[#f3f3f3]">
+    <div class="flex min-h-screen flex-col bg-[#f3f3f3]">
         <PublicNavBar />
         <main class="flex flex-1 items-center justify-center px-4 py-6 -translate-y-7 sm:px-6">
-                <ReserveCard />
-            </main>
+            <ReserveCard />
+        </main>
+        <PublicBottomBar />
     </div>
 </template>
 
