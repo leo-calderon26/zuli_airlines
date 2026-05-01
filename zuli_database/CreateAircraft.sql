d877a0e1b8d3a3d93413a964cace00c7eeed4df9
randy rojas p <randy.rojas@ucr.ac.cr>
2026-05-01 11:01:30 -0600

    Hotfix, Correccion en la creacion de la tabla areonave.

---
+++ zuli_database/CreateAircraft.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Aircraft (
+  aircraftId uniqueidentifier NOT NULL,
+  model varchar(15) unique NOT NULL,
+  weight decimal(10, 4) NOT NULL,
+  adminId uniqueidentifier NOT NULL,
+  numberEconomyClassRows smallint check(numberEconomyClassRows >= 0),
+  numberSeatingRowsEconomy smallint check(numberSeatingRowsEconomy >= 0),
+  numberFirstClassRows smallint check(numberFirstClassRows >= 0),
+  numberSeatingRowsFirst smallint check(numberSeatingRowsFirst >= 0),
+  CONSTRAINT PK_Aicraft PRIMARY KEY(aircraftId),
+  CONSTRAINT FK_User_AdminId FOREIGN KEY(adminId) REFERENCES [User](UserId),
+  CONSTRAINT CK_Model CHECK(model NOT LIKE '% %')
+);
