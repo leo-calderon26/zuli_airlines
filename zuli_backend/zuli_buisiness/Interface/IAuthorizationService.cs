3da94065b9b6d4341d34a98d13b31a070deddd48
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-05-01 14:13:10 -0600

    Se añade el servicio encargado de validar el usuario, crear el token y devolverlo. Capa de negocios

---
+++ zuli_backend/zuli_buisiness/Interface/IAuthorizationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using zuli_Buisiness.DTO.External;
+
+namespace zuli_Buisiness.Interface
+{
+    public interface IAuthorizationService
+    {
+        Task<AuthorizationResponseDTO> ValidateUser(AuthorizationDTO user);
+    }
+}
