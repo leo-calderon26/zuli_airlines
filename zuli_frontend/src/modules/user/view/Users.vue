1ad4aeb9abc55d9f86a0b20b0589cc90b92f3a34
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-05-02 13:37:55 -0600

    Se corrigieron las rutas para los componentes compartidos.

--- zuli_frontend/src/modules/user/view/Users.vue
+++ zuli_frontend/src/modules/user/view/Users.vue
@@ -4,11 +4,12 @@
     <h1 class = "message">Users en mantenimiento</h1>
     <PublicBottomBar />
   </div>
+  <PublicBottomBar />
 </template>
 
 <script>
-import PublicNavBar from '../../../components/PublicNavBar.vue';
-import PublicBottomBar from '../../../components/PublicBottomBar.vue';
+import PublicNavBar from '../../../shared/PublicNavBar.vue';
+import PublicBottomBar from '../../../shared/PublicBottomBar.vue';
 
 export default {
   components: {
