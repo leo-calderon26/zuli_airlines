c1eda0c7b64b2127a7ec2f7de8a56c620980a01b
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-05-02 13:11:33 -0600

    Se realizan ajustes para adaptar la interfaz a tailwind.

--- zuli_frontend/src/modules/airport/view/Airports.vue
+++ zuli_frontend/src/modules/airport/view/Airports.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="landing-page">
     <PublicNavBar />
-    <h1 class = "message">Routes en mantenimiento</h1>
+    <h1 class = "message">Airports en mantenimiento</h1>
     <PublicBottomBar />
   </div>
 </template>
 
 <script>
-import PublicNavBar from '../components/PublicNavBar.vue'
-import PublicBottomBar from '../components/PublicBottomBar.vue'
+import PublicNavBar from '../../../components/PublicNavBar.vue';
+import PublicBottomBar from '../../../components/PublicBottomBar.vue';
 
 export default {
   components: {
