1ad56214589371abde05ce98b388d0d2da54c60e
randy rojas p <randy.rojas@ucr.ac.cr>
2026-05-01 21:55:03 -0600

    Implementacion de la ruta de vuelo en la capa de repository

---
+++ zuli_backend/zuli_Repository/FlightRouteRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using zuli_Data;
+using zuli_Data.Entites;
+using zuli_Data.Entities;
+using zuli_Repository.Interface;
+
+namespace zuli_Repository
+{
+    public class FlightRouteRepository: IFlightRouteRepository
+    {
+        private readonly DapperContext _context;
+
+        public FlightRouteRepository(DapperContext context) => _context = context;
+        public async Task<int> CreateFlightRouter(FlightRouteEntity flightRoute)
+        {
+            using var connection = _context.CreateConnection();
+            const string sql = @"
+                  INSERT INTO FlightRoute
+                    (adminId, airlineId, arrivalAirport, departureAirport, scheduledArrivalTime, scheduledDeparture, frequency, estimatedDuration)
+                  VALUES
+                    (@AdminId, @AirlineId, @ArrivalAirport, @DepartureAirport, @ScheduledArrivalTime, @ScheduledDepartureTime, @Frequency, @EstimatedDuration);
+            ";
+
+            var newId = await connection.ExecuteScalarAsync<int>(sql, new
+            {
+                AdminId = flightRoute.adminId,                
+                AirlineId = flightRoute.airlineId,              
+                ArrivalAirport = flightRoute.arrivalAirport,    
+                DepartureAirport = flightRoute.departureAirport,       
+                ScheduledArrivalTime = flightRoute.scheduledArrivalTime,
+                ScheduledDepartureTime = flightRoute.scheduledDepartureTime,
+                Frequency = flightRoute.frequency,
+                EstimatedDuration = flightRoute.estimatedDuration 
+            });
+            return newId;
+        }
+
+        public async Task<bool> AlreadyExistFlightRoute(FlightRouteEntity flightRoute)
+        {
+            using var connection = _context.CreateConnection();
+
+            const string sql = @"
+                SELECT COUNT(1)
+                FROM FlightRoute
+                WHERE arrivalAirport = @arrivalAirport
+                AND departureAirport = @departureAirport
+                AND frequency = @frequency
+                AND scheduledArrivalTime = @scheduledArrivalTime
+                AND scheduledDeparture = @scheduledDepartureTime
+                AND estimatedDuration = @estimatedDuration";
+
+            var count = await connection.ExecuteScalarAsync<int>(sql, new
+            {
+                flightRoute.arrivalAirport,
+                flightRoute.departureAirport,
+                flightRoute.frequency,
+                flightRoute.scheduledArrivalTime,
+                flightRoute.scheduledDepartureTime,
+                flightRoute.estimatedDuration
+            });
+
+            return count > 0;
+        }
+ 
+    }
+
+}
