07d7fca236a089a705319fdccfe9a83b0c399ab3
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2026-05-02 00:16:56 -0600

    Agregado .vue y base de el formulario de login

--- zuli_frontend/router/index.js
+++ zuli_frontend/router/index.js
@@ -1,1 +1,21 @@
-// aqui es donde vamos hacer las rutas de la page
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../src/views/Login.vue";
+
+const routes = [
+    {
+        path: "/",
+        redirect: "/login"
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
