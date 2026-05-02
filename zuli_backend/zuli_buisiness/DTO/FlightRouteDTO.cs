a8dd6cacaf526a47c1b69609d7df18c83eae4b4b
randy rojas p <randy.rojas@ucr.ac.cr>
2026-05-01 22:13:38 -0600

    Implementación de la entidad y servicio para la ruta de vuelo, incluyendo validaciones y DTOs

---
+++ zuli_backend/zuli_buisiness/DTO/FlightRouteDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using System.ComponentModel.DataAnnotations;
+
+namespace zuli_Buisiness.DTO
+{
+    public class FlightRouteDTO
+    {
+        [Required(ErrorMessage = "La frecuencia del vuelo es requerida")]
+        public int frequecy { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime scheduledArrivalTime { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime scheduledDepartureTime { get; set; }
+        // Este sera calculado por la persona que ingrese las rutas y es en segundos
+        [Required(ErrorMessage = "Es necesario ingresar la duración estimada")]
+        public int estimatedDuration { get; set; }
+        [Required(ErrorMessage = "Es necesario el Id del admin")]
+        public Guid adminId { get; set; }
+        [Required(ErrorMessage = "Es necesario el Id de la aerolinea")]
+        public int airlineId { get; set; }
+        [Required(ErrorMessage = "Es necesario el aeropuerto de llegada")]
+        public string arrivalAirport {  get; set; }
+        [Required(ErrorMessage = "Es necesario el aeropuerto de salida")]
+        public string departureAirport { get; set; }
+    }
+}
---
+++ zuli_backend/zuli_buisiness/Validation/FlightRouteValidator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+using zuli_Buisiness.DTO;
+using zuli_Data.Exceptions;
+
+namespace zuli_Buisiness.Validation
+{
+    public static class FlightRouteAtributes
+    {
+        public const string FREQUECY = "Frequecy";
+        public const string SCHEDULED_ARRIVAL_TIME = "ScheduledArrivalTime";
+        public const string SCHEDULED_DEPARTURE_TIME = "ScheduledDepartureTime";
+        public const string ESTIMATED_DURATION = "EstimatedDuration";
+        public const string AIRLINE_ID = "AirlineId";
+        public const string ARRIVAL_AIRPORT = "ArrivalAirport";
+        public const string DEPARTURE_AIRPORT = "DepartureAirport";
+    }
+    public class FlightRouteValidator
+    {
+        public void ValidationFlightRoute(FlightRouteDTO flightRoute)
+        {
+            var errorInfo = new Dictionary<string, List<string>>()
+            {
+                {FlightRouteAtributes.FREQUECY, new List<string>() },
+                {FlightRouteAtributes.SCHEDULED_ARRIVAL_TIME, new List<string>() },
+                {FlightRouteAtributes.SCHEDULED_DEPARTURE_TIME, new List<string>() },
+                {FlightRouteAtributes.ESTIMATED_DURATION, new List<string>() },
+
+                {FlightRouteAtributes.AIRLINE_ID, new List<string>() },
+                {FlightRouteAtributes.ARRIVAL_AIRPORT, new List<string>() },
+                {FlightRouteAtributes.DEPARTURE_AIRPORT, new List<string>() },
+            };
+            var IsEmptyInfo = false;
+            if (flightRoute.frequecy < 0)
+            {
+                errorInfo[FlightRouteAtributes.FREQUECY].Add("La frecuencia del vuelo tiene que ser un numero entero positivo");
+                IsEmptyInfo = true;
+            }
+            if (flightRoute.estimatedDuration < 0)
+            {
+                errorInfo[FlightRouteAtributes.ESTIMATED_DURATION].Add("La duración estimada debe de ser un numero positivo");
+                IsEmptyInfo = true;
+            }
+            if (string.IsNullOrWhiteSpace(flightRoute.arrivalAirport))
+            {
+                errorInfo[FlightRouteAtributes.ARRIVAL_AIRPORT].Add("Es necesario ingresar el aeropuerto de llegada");
+                IsEmptyInfo = true;
+            }
+            if (string.IsNullOrWhiteSpace(flightRoute.departureAirport))
+            {
+                errorInfo[FlightRouteAtributes.DEPARTURE_AIRPORT].Add("Es necesario ingresar el aeropuerto de salida");
+                IsEmptyInfo = true;
+            }
+
+            if (flightRoute.airlineId <= 0)
+            {
+                errorInfo[FlightRouteAtributes.AIRLINE_ID].Add("El airlineId debe ser un numero entero positivo");
+                IsEmptyInfo = true;
+            }
+            if (IsEmptyInfo)
+            {
+                throw new ZuliValidationException(errorInfo.Where(x => x.Value.Count > 0).ToDictionary(x => x.Key, x => x.Value));
+            }
+            else
+            {
+                if (flightRoute.arrivalAirport.Length > 3 || flightRoute.arrivalAirport.Length < 3)
+                {
+                    errorInfo[FlightRouteAtributes.ARRIVAL_AIRPORT].Add("El nombre del aeropuerto de destino tiene que estar en un rango de 0-3 caracteres");
+                }
+                if (flightRoute.departureAirport.Length > 3 || flightRoute.departureAirport.Length < 3)
+                {
+                    errorInfo[FlightRouteAtributes.DEPARTURE_AIRPORT].Add("El nombre del aeropuerto de salida tiene que estar en un rango de 0-3 caracteres");
+                }
+                if (errorInfo[FlightRouteAtributes.ARRIVAL_AIRPORT].Count > 0 || errorInfo[FlightRouteAtributes.DEPARTURE_AIRPORT].Count > 0)
+                    throw new ZuliValidationException(errorInfo.Where(x => x.Value.Count > 0).ToDictionary(x => x.Key, x => x.Value));
+            }
+        }
+
+
+    }
+
+}
