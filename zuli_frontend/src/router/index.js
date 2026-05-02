61edcf8fde57666815aaa5ba8648c288b3accdb7
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-05-02 13:11:33 -0600

    Se realizan ajustes para adaptar la interfaz a tailwind.

--- zuli_frontend/src/router/index.js
+++ zuli_frontend/src/router/index.js
@@ -8,23 +8,8 @@ import Routes from '../modules/route/view/Routes.vue'
 import Users from '../modules/user/view/Users.vue'
 import ProfileSettings from '../modules/profile/view/ProfileSettings.vue'
 import Reports from '../modules/reports/view/Reports.vue'
-
 import Login from"../modules/auth/view/Login.vue"
 import authService from "../modules/auth/services/authService";
-
-import ReserveView from '../modules/landing/view/ReserveView.vue'
-import CheckInView from '../modules/landing/view/CheckInView.vue'
-import ConsultView from '../modules/landing/view/ConsultView.vue'
-import HelpView from '../modules/landing/view/HelpView.vue'
-
-import Login from"../modules/auth/view/Login.vue"
-import authService from "../modules/auth/services/authService";
-
-import ReserveView from '../modules/landing/view/ReserveView.vue'
-import CheckInView from '../modules/landing/view/CheckInView.vue'
-import ConsultView from '../modules/landing/view/ConsultView.vue'
-import HelpView from '../modules/landing/view/HelpView.vue'
-
 const router = createRouter({
     history: createWebHistory(),
     routes: [
