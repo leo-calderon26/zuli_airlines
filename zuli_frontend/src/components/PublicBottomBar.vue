9f37c5de87e79ee0d3821b8fed154fe7147339cb
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-05-02 13:20:12 -0600

    Se adaptó el menú y la barra inferior a tailwind.

--- zuli_frontend/src/components/PublicBottomBar.vue
+++ zuli_frontend/src/components/PublicBottomBar.vue
@@ -1,9 +1,21 @@
 <template>
-  <footer class="public-bottom-bar">
-    <div class="bottom-content">
-        <div>© 2026 Zuli Airplanes. Todos los derechos reservados.</div>
-        <div>Tel. (506) 1234-5678</div>
-    </div>
+  <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-primary border-t border-default shadow-sm md:flex md:items-center md:justify-between md:p-6">
+      <span class="text-sm text-body sm:text-center">© 2026 <a href="https://flowbite.com/" class="hover:underline">Zuli airlines™</a>. Todos los derechos reservados.
+      </span>
+      <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-body sm:mt-0">
+          <li>
+              <a href="#" class="hover:underline me-4 md:me-6">About</a>
+          </li>
+          <li>
+              <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+          </li>
+          <li>
+              <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+          </li>
+          <li>
+              <a href="#" class="hover:underline">Contact</a>
+          </li>
+      </ul>
   </footer>
 </template>
 
