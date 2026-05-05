5a6578283b0371bf12220c518e8d2e55200d6943
Goldsarchery <luisalf.ag18@gmail.com>
2026-05-04 21:47:10 -0600

    Endpoint habilitado para busqueda de vuelos con registro de servicios, controlador y capa de negocio

---
+++ zuli_backend/zuli_backend/Controllers/FlightSearchController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using zuli_Buisiness.DTO;
+using zuli_Buisiness.Interface;
+
+namespace zuli_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightController : ControllerBase
+    {
+        private readonly IFlightService _flightService;
+
+        public FlightController(IFlightService flightService) => _flightService = flightService;
+
+        [HttpGet("search")]
+        public async Task<ActionResult<PagedFlightResponseDTO>> SearchFlights([FromQuery] FlightSearchRequestDTO request)
+        {
+            var result = await _flightService.Search(request);
+            return Ok(result); 
+        }
+    }
+}
