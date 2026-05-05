3e890fbf2b072544ba998a0af469814f205a8132
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-05-05 15:13:44 -0600

    Se añade un router guard en el frontend para las rutas del panel administrativo

--- zuli_frontend/src/router/index.js
+++ zuli_frontend/src/router/index.js
@@ -20,92 +20,98 @@ import CheckInView from '../modules/landing/view/CheckInView.vue'
 import ConsultView from '../modules/landing/view/ConsultView.vue'
 import HelpView from '../modules/landing/view/HelpView.vue'
 
-import Login from"../modules/auth/view/Login.vue"
-import authService from "../modules/auth/services/authService";
-
-import ReserveView from '../modules/landing/view/ReserveView.vue'
-import CheckInView from '../modules/landing/view/CheckInView.vue'
-import ConsultView from '../modules/landing/view/ConsultView.vue'
-import HelpView from '../modules/landing/view/HelpView.vue'
-
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: "/",
             name: "reserve",
-            component: ReserveView
+            component: ReserveView,
+            meta: {requiresAuth: false, adminRequired: false}
         },
         {
             path: "/admin/",
             name: "mainMenu",
-            component: MainMenu
+            component: MainMenu,
+            meta: {requiresAuth: true, adminRequired: false}
         },
         {
             path: "/admin/aircrafts/",
             name: "aircraftList",
-            component: AircraftList
+            component: AircraftList,
+            meta: {requiresAuth: true, adminRequired: false}
         },
         {
             path: "/admin/create-aircraft",
             name: "createAircraft",
-            component: AircraftCreate
+            component: AircraftCreate,
+            meta: {requiresAuth: true, adminRequired: true}
         },
         {
             path: "/check-in",
             name: "checkin",
-            component: CheckInView
+            component: CheckInView,
+            meta: {requiresAuth: false, adminRequired: false}
         },
         {
             path: "/consulta",
             name: "consult",
-            component: ConsultView
+            component: ConsultView,
+            meta: {requiresAuth: false, adminRequired: false}
         },
         {
             path: "/ayuda",
             name: "help",
-            component: HelpView
+            component: HelpView,
+            meta: {requiresAuth: false, adminRequired: false}
         },
         {
             path: "/admin/airports",
             name: "airports",
-            component: Airports
+            component: Airports,
+            meta: {requiresAuth: true, adminRequired: false}
         },
         {
             path: "/admin/routes",
             name: "routes",
-            component: Routes
+            component: Routes,
+            meta: {requiresAuth: true, adminRequired: false}
         },
          {
             path: "/admin/flights",
             name: "flights",
-            component: Flights
+            component: Flights,
+            meta: {requiresAuth: true, adminRequired: false}
         },
         {
             path: "/admin/users",
             name: "users",
-            component: Users
+            component: Users,
+            meta: {requiresAuth: true, adminRequired: false}
         },
         {
             path: "/admin/reports",
             name: "reports",
-            component: Reports
+            component: Reports,
+            meta: {requiresAuth: true, adminRequired: false}
         },
         {
             path: "/admin/profile-settings",
             name: "profileSettings",
-            component: ProfileSettings
+            component: ProfileSettings,
+            meta: {requiresAuth: true, adminRequired: false}
         },
         {
             path: "/administrativo",
             name: "administrativo",
-            component: Login
+            component: Login,
+            meta: {requiresAuth: false, adminRequired: false}
         }
     ]
 });
 
 router.beforeEach(async (to, from, next) => {
-    if (!to.meta.requiresAuth) {
+    if (to.meta.requiresAuth === false) {
         next();
         return;
     }
@@ -115,6 +121,24 @@ router.beforeEach(async (to, from, next) => {
         next();
     } catch {
         next({ name: "administrativo" });
+    }
+});
+
+router.beforeEach(async (to, from, next) => {
+    if (to.meta.adminRequired === false) {
+        next();
+        return;
+    }
+    try {
+        var data = await authService.me();
+        if (data.userRole === 'Administrator') {
+            next();
+            return;
+        }
+        else
+            next(from);
+    } catch {
+        next(from);
     }
 });
 
