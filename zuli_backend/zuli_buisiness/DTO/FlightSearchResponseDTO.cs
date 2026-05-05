20ec252599576b0c2da81ef454e58d76099bf5d7
Goldsarchery <luisalf.ag18@gmail.com>
2026-05-04 18:53:34 -0600

    Crear DTOs solicitud y respuesta para buscar vuelos

---
+++ zuli_backend/zuli_buisiness/DTO/FlightSearchResponseDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace zuli_Buisiness.DTO
+{
+    public class FlightSearchResponseDTO
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string DepartureTimeText { get; set; }
+        public string ArrivalTimeText { get; set; }
+        public string ArrivalDateText { get; set; }
+        public string TotalDurationText { get; set; }
+        public int Stops { get; set; }
+        public string LayoverAirports { get; set; }
+        public decimal TotalTouristPrice { get; set; }
+        public decimal TotalFirstClassPrice { get; set; }
+    }
+
+    public class PagedFlightResponseDTO
+    {
+        public int TotalRecords { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public List<FlightSearchResponseDTO> Flights { get; set; }
+    }
+}
