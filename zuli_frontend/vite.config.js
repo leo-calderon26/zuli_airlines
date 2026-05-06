3aefb66846db1782b7124770ba420aa5e07823c9
randy rojas p <randy.rojas@ucr.ac.cr>
2026-05-06 12:39:08 -0600

    Pequeños cambios en los puertos

--- zuli_frontend/vite.config.js
+++ zuli_frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'http://localhost:7034',
         changeOrigin: true,
         secure: false,
       },
