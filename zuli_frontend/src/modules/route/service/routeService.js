9eade448117b05946366fed945b8c7f287c6cb06
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-05-06 12:07:42 -0600

    Se actualizan las rutas en el router, en las vistas y en el controlador.

--- zuli_frontend/src/modules/route/service/routeService.js
+++ zuli_frontend/src/modules/route/service/routeService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const ROUTE_API_URL = "/api/FlightRoute";
-const AIRPORT_API_URL = "/api/Airport";
+const ROUTE_API_URL = "/api/admin/FlightRoute";
+const AIRPORT_API_URL = "/api/admin/Airport";
 
 export const createFlightRoute = async (routeData) => {
   try {
-    const response = await axios.post(`${ROUTE_API_URL}/CreateFlightRouter`, routeData);
+    const response = await axios.post(`${ROUTE_API_URL}/CreateFlightRoute`, routeData);
     return response.data;
   } catch (error) {
     console.error("Error al crear ruta:", error);
