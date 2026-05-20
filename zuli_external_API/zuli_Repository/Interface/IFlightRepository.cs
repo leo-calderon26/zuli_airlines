73a64379ede03b99623011105c92ea551576a13f
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-05-20 12:26:15 -0600

    Se mueven los archivos correspondientes a la API externa a un nuevo proyecto.

--- zuli_external_API/zuli_Repository/Interface/IFlightRepository.cs
+++ zuli_external_API/zuli_Repository/Interface/IFlightRepository.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using zuli_Data.Entities.External;
+using zuli_Data.Entities;
 
 namespace zuli_Repository.Interface
 {
-    public interface IExternalFlightRepository
+    public interface IFlightRepository
     {
         Task<IEnumerable<RetrievedFlightEntity>> RetrieveAvailableFlights(RequestedFlightEntity requestedFlight);
     }
---
+++ zuli_external_API/zuli_backend/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,73 @@
+﻿
+using zuli_Business;
+using zuli_Data.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Diagnostics;
+
+namespace zuli_external_API.Middleware
+{
+    public class GlobalExceptionMiddleware
+    {
+        // Esta variable se usa para pasar al siguente middleware sino esto se queda pegado
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+        // TODO(you) hacer que los errores se guarden en unos logs si es que los profes lo piden
+        // recuerde que el middleware es un singleton
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            } 
+            catch(AppExceptions exeption)
+            {
+                _logger.LogWarning(exeption, "App exception: {Code}", exeption.ErrorCode);
+                await WriteProblemAsync(
+                    context,
+                    exeption.StatusCode,
+                    exeption.ErrorCode,
+                    exeption.Message,
+                    exeption,
+                    exeption.ErrorId
+                    );
+            }
+            catch(Exception exeption)
+            {
+                // Error no controlado
+                _logger.LogError(exeption, "unhandled error");
+                // TODO(you) Aqui se debe de crear una forma para guardar los logs del error de 
+                // forma que siga funcionando
+                await WriteProblemAsync(context, StatusCodes.Status500InternalServerError, 
+                    "INTERNAL_ERROR", "An unexpected error occurred.");
+            }
+        }
+        private async Task WriteProblemAsync(HttpContext context, int statusCode, 
+            string errorCode, string detail, AppExceptions? appEx = null, int ErrorId = -1)
+        {
+            context.Response.ContentType = "application/problem+json";
+            context.Response.StatusCode =statusCode;
+            var error = new ProblemDetails
+            {
+                Status = statusCode,
+                Title = ReasonPhrases.GetReasonPhrase(statusCode),
+                Detail = detail,
+                Instance = context.Request.Path
+            };
+            error.Extensions["errorCode"] = errorCode;
+            error.Extensions["traceId"] = Activity.Current?.Id ?? context.TraceIdentifier;
+
+            if (appEx is ZuliValidationException ve)
+                error.Extensions["errors"] = ve.Errors;
+
+            await context.Response.WriteAsJsonAsync(error);
+        }
+    }
+}
