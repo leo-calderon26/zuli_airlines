ee0c7e02a9ff25db1357041dbb19e0bffdd7d7bd
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2026-05-26 19:39:18 -0600

    Creación de DTO's para confirmación de compra

---
+++ zuli_backend/zuli_buisiness/DTO/PurchaseConfirmationFlightDTO.cs
@@ -0,0 +1,18 @@
+namespace zuli_Business.DTO.PurchaseConfirmation
+{
+    public class PurchaseConfirmationFlightDTO
+    {
+        public int FlightId { get; set; }
+        public string FlightNumber { get; set; } = string.Empty;
+        public string AirlineName { get; set; } = string.Empty;
+
+        public string OriginAirportName { get; set; } = string.Empty;
+        public string OriginAirportCode { get; set; } = string.Empty;
+
+        public string DestinationAirportName { get; set; } = string.Empty;
+        public string DestinationAirportCode { get; set; } = string.Empty;
+
+        public DateTime DepartureDateTime { get; set; }
+        public DateTime ArrivalDateTime { get; set; }
+    }
+}
