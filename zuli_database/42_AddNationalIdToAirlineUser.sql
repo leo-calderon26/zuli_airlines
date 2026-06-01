87261b8b9b70be1e57f922de921bc2d034e1f860
randyrojasp <randy.rojas@ucr.ac.cr>
2026-05-31 20:01:40 -0600

    Alter de la tabla AirlineUser

--- zuli_database/42_AddNationalIdToAirlineUser.sql
+++ zuli_database/42_AddNationalIdToAirlineUser.sql
@@ -1,5 +1,19 @@
 USE ZuliAirlines;
 GO
+
 ALTER TABLE dbo.AirlineUser
-ADD NationalId CHAR(9) NOT NULL UNIQUE;
+ADD NationalId CHAR(9) NULL;
+GO
+
+UPDATE dbo.AirlineUser 
+SET NationalId = '000000000' 
+WHERE NationalId IS NULL;
+GO
+
+ALTER TABLE dbo.AirlineUser
+ALTER COLUMN NationalId CHAR(9) NOT NULL;
+GO
+
+ALTER TABLE dbo.AirlineUser
+ADD CONSTRAINT UQ_AirlineUser_NationalId UNIQUE (NationalId);
 GO
