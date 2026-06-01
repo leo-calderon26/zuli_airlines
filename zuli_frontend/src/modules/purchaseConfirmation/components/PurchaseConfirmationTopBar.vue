eee6a0a0b0f0003524cfd14280fd488ba62df332
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2026-06-01 13:57:33 -0600

    Arreglos a la topbar, boton Zuli

--- zuli_frontend/src/modules/purchaseConfirmation/components/PurchaseConfirmationTopBar.vue
+++ zuli_frontend/src/modules/purchaseConfirmation/components/PurchaseConfirmationTopBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="h-[66px] w-full bg-primary">
-    <div class="mx-auto flex h-full max-w-7xl items-center px-6">
+  <header class="flex h-[84px] w-full items-center bg-primary px-8 text-white">
+    <div class="flex w-52 shrink-0 items-center">
       <button
         type="button"
         class="flex items-center"
@@ -9,28 +9,28 @@
         <img
           src="../../../assets/logoZuli.svg"
           alt="Zuli Airlines"
-          class="h-[48px] w-auto"
+          class="h-20 w-auto object-contain"
         >
       </button>
-
-      <nav class="ml-8 flex items-center gap-10">
-        <button
-          type="button"
-          class="text-[16px] font-medium text-white transition hover:opacity-80"
-          @click="$emit('goDestination')"
-        >
-          Destino
-        </button>
-
-        <button
-          type="button"
-          class="text-[16px] font-medium text-white transition hover:opacity-80"
-          @click="$emit('goReservation')"
-        >
-          Reserva
-        </button>
-      </nav>
     </div>
+
+    <nav class="flex items-center gap-10">
+      <button
+        type="button"
+        class="text-[16px] font-medium text-white transition hover:opacity-80"
+        @click="$emit('goDestination')"
+      >
+        Destino
+      </button>
+
+      <button
+        type="button"
+        class="text-[16px] font-medium text-white transition hover:opacity-80"
+        @click="$emit('goReservation')"
+      >
+        Reserva
+      </button>
+    </nav>
   </header>
 </template>
 
