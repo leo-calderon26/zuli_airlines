115f810928a9f56cb42666ce4b537befa99b464a
GeinerMB <monba3122@gmail.com>
2026-06-21 22:25:51 -0600

    Busqueda sin filtros funcionando

--- zuli_backend/zuli_Repository/FlightsReportRepository.cs
+++ zuli_backend/zuli_Repository/FlightsReportRepository.cs
@@ -22,28 +22,43 @@ namespace zuli_Repository
         {
             using var connection = _context.CreateConnection();
 
-            // La consulta limpia basada 100% en lo que te funcionó en SSMS
+
             var sql = @"
-                SELECT 
-                    f.FlightDate AS Fecha,
-                    f.RealDepartureAirport AS Origen,
-                    f.RealArrivalAirport AS Destino,
-                    f.FlightRouteId AS NumeroVuelo,
-                    a.AirlineName AS Aerolinea,
-                    
-                    -- Lógica simple: si la fila tiene la clase, sumamos 1, si no 0
-                    CASE WHEN r.FlightClass = 'FirstClass' THEN 1 ELSE 0 END AS PasajerosPrimera,
-                    CASE WHEN r.FlightClass = 'Turista' OR r.FlightClass = 'Economy' THEN 1 ELSE 0 END AS PasajerosEconomica,
-                    
-                    -- Dinero crudo por fila
-                    ISNULL(r.TotalPayment, 0.00) AS VentaPasajeros,
-                    (ISNULL(f.CarryOnPrice, 0.00) + ISNULL(f.CheckedPrice, 0.00)) AS VentaEquipaje,
-                    (ISNULL(r.TotalPayment, 0.00) + ISNULL(f.CarryOnPrice, 0.00) + ISNULL(f.CheckedPrice, 0.00)) AS TotalVenta
-                FROM dbo.Flight f
-                INNER JOIN dbo.FlightRoute fr ON f.FlightRouteId = fr.FlightRouteId
-                INNER JOIN dbo.Airline a ON fr.AirlineId = a.AirlineId
-                LEFT JOIN dbo.BoardingPass bp ON f.Id = bp.FlightId
-                LEFT JOIN dbo.Reservation r ON bp.ReservationCode = r.ReservationCode;";
+            SELECT 
+            f.FlightDate AS Fecha,
+            f.RealDepartureAirport AS Origen,
+            f.RealArrivalAirport AS Destino,
+            f.FlightRouteId AS NumeroVuelo,
+            a.AirlineName AS Aerolinea,
+            
+            SUM(CASE WHEN r.FlightClass = 'Primera Clase' THEN 1 ELSE 0 END) AS PasajerosPrimera,
+            SUM(CASE WHEN r.FlightClass = 'Turista' OR r.FlightClass = 'Economy' THEN 1 ELSE 0 END) AS PasajerosEconomica,
+            
+            (SUM(CASE WHEN r.FlightClass = 'Primera Clase' THEN 1 ELSE 0 END) * f.FirstClassPrice) +
+            (SUM(CASE WHEN r.FlightClass = 'Turista' OR r.FlightClass = 'Economy' THEN 1 ELSE 0 END) * f.TouristPrice) AS VentaPasajeros,
+            
+            SUM(CASE WHEN bp.FlightId IS NOT NULL THEN (ISNULL(f.CarryOnPrice, 0.00) + ISNULL(f.CheckedPrice, 0.00)) ELSE 0.00 END) AS VentaEquipaje,
+            
+            ((SUM(CASE WHEN r.FlightClass = 'Primera Clase' THEN 1 ELSE 0 END) * f.FirstClassPrice) +
+             SUM(CASE WHEN r.FlightClass = 'Turista' OR r.FlightClass = 'Economy' THEN 1 ELSE 0 END) * f.TouristPrice) +
+            SUM(CASE WHEN bp.FlightId IS NOT NULL THEN (ISNULL(f.CarryOnPrice, 0.00) + ISNULL(f.CheckedPrice, 0.00)) ELSE 0.00 END) AS TotalVenta
+            
+        FROM dbo.Flight f
+        INNER JOIN dbo.FlightRoute fr ON f.FlightRouteId = fr.FlightRouteId
+        INNER JOIN dbo.Airline a ON fr.AirlineId = a.AirlineId
+        LEFT JOIN dbo.BoardingPass bp ON f.Id = bp.FlightId
+        LEFT JOIN dbo.Reservation r ON bp.reservationCode = r.reservationCode
+        
+        GROUP BY 
+            f.Id, 
+            f.FlightDate,
+            f.RealDepartureAirport,
+            f.RealArrivalAirport,
+            f.FlightRouteId,
+            a.AirlineName,
+            f.FirstClassPrice,  
+            f.TouristPrice;
+";
 
 
             var report = await connection.QueryAsync<FlightsReportEntity>(sql);
