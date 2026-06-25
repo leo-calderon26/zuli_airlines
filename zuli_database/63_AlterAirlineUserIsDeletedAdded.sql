cd61828bd062f516857905c1ab66eacc93e2f7c3
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2026-06-25 15:10:57 -0600

    Create 63_AlterUserIsDeletedAdded.sql
    IsDeletedAdded&FixesToScript

---
+++ zuli_database/63_AlterAirlineUserIsDeletedAdded.sql
@@ -0,0 +1,11 @@
+﻿USE ZuliAirlines;
+GO
+
+IF COL_LENGTH('dbo.AirlineUser', 'IsDeleted') IS NULL
+BEGIN
+    ALTER TABLE dbo.AirlineUser
+    ADD IsDeleted BIT NOT NULL
+        CONSTRAINT DF_AirlineUser_IsDeleted
+        DEFAULT (0) WITH VALUES;
+END;
+GO
