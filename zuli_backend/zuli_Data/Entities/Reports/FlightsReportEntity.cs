0d8ccb362f383ef542aeea0c3b8d268aec572b02
GeinerMB <monba3122@gmail.com>
2026-06-25 14:32:18 -0600

    Cambio de idioma solicitado en Entity y DTO

--- zuli_backend/zuli_Data/Entities/Reports/FlightsReportEntity.cs
+++ zuli_backend/zuli_Data/Entities/Reports/FlightsReportEntity.cs
@@ -4,15 +4,15 @@ namespace zuli_Data.Entities.Reports
 {
     public class FlightsReportEntity
     {
-        public DateTime Fecha { get; set; }
-        public string Origen { get; set; } = string.Empty;
-        public string Destino { get; set; } = string.Empty;
-        public int NumeroVuelo { get; set; }
-        public int PasajerosPrimera { get; set; }
-        public int PasajerosEconomica { get; set; }
-        public string Aerolinea { get; set; } = string.Empty;
-        public decimal VentaPasajeros { get; set; }
-        public decimal VentaEquipaje { get; set; }
-        public decimal TotalVenta { get; set; }
+        public DateTime Date { get; set; }
+        public string Origin { get; set; } = string.Empty;
+        public string Destination { get; set; } = string.Empty;
+        public int FlightNumber { get; set; }
+        public int FirstClassPassengers { get; set; }
+        public int EconomyClassPassengers { get; set; }
+        public string Airline { get; set; } = string.Empty;
+        public decimal PassengerSales { get; set; }
+        public decimal BaggageSales { get; set; }
+        public decimal TotalSales { get; set; }
     }
 }
