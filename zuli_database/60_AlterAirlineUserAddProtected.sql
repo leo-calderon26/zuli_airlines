f272de7b563bf5133442649492ebd072a7cffd5d
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2026-06-24 21:16:45 -0600

    Cambios para proteccion de eliminacion

---
+++ zuli_database/60_AlterAirlineUserAddProtected.sql
@@ -0,0 +1,11 @@
+﻿USE ZuliAirlines;
+GO
+
+IF COL_LENGTH('dbo.AirlineUser', 'IsProtected') IS NULL
+BEGIN
+    ALTER TABLE dbo.AirlineUser
+    ADD IsProtected BIT NOT NULL
+        CONSTRAINT DF_AirlineUser_IsProtected
+        DEFAULT (0) WITH VALUES;
+END;
+GO
---
+++ zuli_database/61_RootStatusProtectedAdded.sql
@@ -0,0 +1,30 @@
+﻿USE ZuliAirlines;
+GO
+
+DECLARE @rootEmail VARCHAR(254) = 'admin@zuliairlines.com';
+
+IF
+(
+    SELECT COUNT(*)
+    FROM dbo.AirlineUser
+    WHERE BusinessEmail = @rootEmail
+) <> 1
+BEGIN
+    THROW 50010, 'No se encontró exactamente un usuario root con el correo configurado.', 1;
+END;
+
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.AirlineUser
+    WHERE BusinessEmail = @rootEmail
+      AND UserRole <> 'Administrator'
+)
+BEGIN
+    THROW 50011, 'El usuario root configurado debe tener rol Administrator.', 1;
+END;
+
+UPDATE dbo.AirlineUser
+SET IsProtected = 1
+WHERE BusinessEmail = @rootEmail;
+GO
