f63e3341ef4ae65a0afae1186169752fbff6f52f
GeinerMB <monba3122@gmail.com>
2026-06-25 13:57:32 -0600

    Descarga de archivo excell funcional

--- zuli_backend/zuli_backend/Program.cs
+++ zuli_backend/zuli_backend/Program.cs
@@ -218,6 +218,7 @@ builder.Services.AddScoped<IFlightsReportExportService, FlightsReportExportServi
 
 builder.Services.AddScoped<IFlightsReportRepository, FlightsReportRepository>();
 builder.Services.AddScoped<IFlightsReportService, FlightsReportService>();
+builder.Services.AddScoped<IFlightsReportExportService, FlightsReportExportService>();
 
 QuestPDF.Settings.License = LicenseType.Community;
 
