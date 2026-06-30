7fe413b2c66a98967711fe0b928dc63e78857c6b
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-06-30 11:55:15 -0600

    Se resuelven los comentarois realizados por los compañeros

--- zuli_backend/zuli_buisiness/Validation/OutsideFlightSearchValidator.cs
+++ zuli_backend/zuli_buisiness/Validation/OutsideFlightSearchValidator.cs
@@ -7,11 +7,6 @@ namespace zuli_Business.Validation
     {
         public OutsideFlightSearchValidator()
         {
-            RuleFor(x => x.Destination)
-                .NotEmpty().Length(3)
-                .WithMessage("El destino debe tener exactamente 3 caracteres.");
-            RuleFor(x => x.QuantityOfPassengers).GreaterThan(0)
-                .WithMessage("Debe buscar al menos 1 asiento.");
             RuleFor(x => x.EarliestDeparture)
                 .GreaterThanOrEqualTo(_ => DateTime.Today)
                 .WithMessage("La fecha de salida no puede ser anterior a hoy.");
