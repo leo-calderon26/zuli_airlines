1ee476eaeeb6578f5a6047aad3463296a6df388d
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-06-30 18:13:15 -0600

    Se ajustan las entities para procesar información extra de las aerolíneas externas

--- zuli_backend/zuli_Data/Entities/OutsideFlightEntity.cs
+++ zuli_backend/zuli_Data/Entities/OutsideFlightEntity.cs
@@ -9,11 +9,15 @@ namespace zuli_Data.Entities
         public decimal FirstClassPrice { get; set; }
         public TimeSpan RealDepartureTime { get; set; }
         public TimeSpan RealArrivalTime { get; set; }
-        public int DurationOnMinutes { get; set; }
+        public int DurationOnSeconds { get; set; }
         public decimal? CarryOnPrice { get; set; }
         public decimal? CheckedPrice { get; set; }
         public string ArrivalAirportCode { get; set; }
+        public string ArrivalAirportName { get; set; }
+        public string ArrivalAirportCity { get; set; }
         public string DepartureAirportCode { get; set; }
+        public string DepartureAirportName { get; set; }
+        public string DepartureAirportCity { get; set; }
         public int Frequency { get; set; }
     }
 }
