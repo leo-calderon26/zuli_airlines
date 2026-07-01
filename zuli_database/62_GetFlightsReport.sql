d63b730b40999016887dcf9b4b935b42a2aead34
GeinerMB <monba3122@gmail.com>
2026-06-30 20:37:38 -0600

    correccion en idioma de GetFlightsReport

--- zuli_database/62_GetFlightsReport.sql
+++ zuli_database/62_GetFlightsReport.sql
@@ -12,22 +12,22 @@ RETURN
 (
     SELECT
     f.FlightDate AS Fecha,
-    f.RealDepartureAirport AS Origen,
-    f.RealArrivalAirport AS Destino,
-    f.FlightRouteId AS NumeroVuelo,
+    f.RealDepartureAirport AS Origin,
+    f.RealArrivalAirport AS Destination,
+    f.FlightRouteId AS FlightNumber,
     a.AirlineName AS Aerolinea,
 
-    SUM(CASE WHEN r.FlightClass = 'Primera Clase' THEN 1 ELSE 0 END) AS PasajerosPrimera,
-    SUM(CASE WHEN r.FlightClass = 'Turista' OR r.FlightClass = 'Economy' THEN 1 ELSE 0 END) AS PasajerosEconomica,
+    SUM(CASE WHEN r.FlightClass = 'Primera Clase' THEN 1 ELSE 0 END) AS FirstClassPassengers,
+    SUM(CASE WHEN r.FlightClass = 'Turista' OR r.FlightClass = 'Economy' THEN 1 ELSE 0 END) AS EconomyPassengers,
 
     (SUM(CASE WHEN r.FlightClass = 'Primera Clase' THEN 1 ELSE 0 END) * f.FirstClassPrice) +
-        (SUM(CASE WHEN r.FlightClass = 'Turista' OR r.FlightClass = 'Economy' THEN 1 ELSE 0 END) * f.TouristPrice) AS VentaPasajeros,
+        (SUM(CASE WHEN r.FlightClass = 'Turista' OR r.FlightClass = 'Economy' THEN 1 ELSE 0 END) * f.TouristPrice) AS PassengerSales,
 
-    SUM(CASE WHEN bp.FlightId IS NOT NULL THEN (ISNULL(f.CarryOnPrice, 0.00) + ISNULL(f.CheckedPrice, 0.00)) ELSE 0.00 END) AS VentaEquipaje,
+    SUM(CASE WHEN bp.FlightId IS NOT NULL THEN (ISNULL(f.CarryOnPrice, 0.00) + ISNULL(f.CheckedPrice, 0.00)) ELSE 0.00 END) AS BaggageSales,
 
     ((SUM(CASE WHEN r.FlightClass = 'Primera Clase' THEN 1 ELSE 0 END) * f.FirstClassPrice) +
          SUM(CASE WHEN r.FlightClass = 'Turista' OR r.FlightClass = 'Economy' THEN 1 ELSE 0 END) * f.TouristPrice) +
-        SUM(CASE WHEN bp.FlightId IS NOT NULL THEN (ISNULL(f.CarryOnPrice, 0.00) + ISNULL(f.CheckedPrice, 0.00)) ELSE 0.00 END) AS TotalVenta
+        SUM(CASE WHEN bp.FlightId IS NOT NULL THEN (ISNULL(f.CarryOnPrice, 0.00) + ISNULL(f.CheckedPrice, 0.00)) ELSE 0.00 END) AS TotalSales
 
 FROM dbo.Flight f
     INNER JOIN dbo.FlightRoute fr ON f.FlightRouteId = fr.FlightRouteId
