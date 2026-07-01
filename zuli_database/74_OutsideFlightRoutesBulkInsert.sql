5c4d70b89ff54694c5cea9d454a0425e3db2b0a9
Leonardo <LEONARDO.CALDERONRIVERA@ucr.ac.cr>
2026-07-01 17:16:35 -0600

    Se añade la transacción al OutsideFlightRoutesBulkInsert

--- zuli_database/74_OutsideFlightRoutesBulkInsert.sql
+++ zuli_database/74_OutsideFlightRoutesBulkInsert.sql
@@ -22,62 +22,64 @@ CREATE OR ALTER PROCEDURE dbo.sp_BulkOutsideFlightRoutes
     @OutsideFlightRoutes dbo.OutsideFlightRouteBulkType READONLY
 AS
 BEGIN
-    SET NOCOUNT ON;
-    declare @adminId uniqueidentifier;
-    set @adminId = (SELECT UserId from AirlineUser WHERE BusinessID = '100000000');
-    declare @aircraftId uniqueidentifier;
-    set @aircraftId = (SELECT AircraftId from Aircraft WHERE Model = 'Yigui-Externo');
-    WITH Airports AS (
-        SELECT DISTINCT ArrivalAirportCode AS AirportCode,
-                ArrivalAirportName AS Name,
-                ArrivalAirportCity AS City
-        FROM @OutsideFlightRoutes
+    BEGIN TRAN
+        SET NOCOUNT ON;
+        declare @adminId uniqueidentifier;
+        set @adminId = (SELECT UserId from AirlineUser WHERE BusinessID = '100000000');
+        declare @aircraftId uniqueidentifier;
+        set @aircraftId = (SELECT AircraftId from Aircraft WHERE Model = 'Yigui-Externo');
+        WITH Airports AS (
+            SELECT DISTINCT ArrivalAirportCode AS AirportCode,
+                    ArrivalAirportName AS Name,
+                    ArrivalAirportCity AS City
+            FROM @OutsideFlightRoutes
 
-        UNION
+            UNION
 
-        SELECT DISTINCT DepartureAirportCode,
-                DepartureAirportName,
-                DepartureAirportCity
-        FROM @OutsideFlightRoutes
-    )
-    INSERT INTO dbo.Airport (AirportCode, Name, Country, City, AdminId)
-    SELECT a.AirportCode, a.Name, a.City, a.City, @adminId
-    FROM Airports a
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM dbo.Airport ap
-        WHERE ap.AirportCode = a.AirportCode
-    );
+            SELECT DISTINCT DepartureAirportCode,
+                    DepartureAirportName,
+                    DepartureAirportCity
+            FROM @OutsideFlightRoutes
+        )
+        INSERT INTO dbo.Airport (AirportCode, Name, Country, City, AdminId)
+        SELECT a.AirportCode, a.Name, a.City, a.City, @adminId
+        FROM Airports a
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM dbo.Airport ap
+            WHERE ap.AirportCode = a.AirportCode
+        );
 
-    INSERT INTO dbo.FlightRoute(AdminId, AirlineId, ArrivalAirport, DepartureAirport, ScheduledArrivalTime, ScheduledDepartureTime, Frequency, EstimatedDuration, AircraftId, CarryOnPrice, CheckedPrice, TouristPrice, FirstClassPrice)
-    SELECT @adminId as AdminId, AirlineId, ArrivalAirportCode as ArrivalAirport, DepartureAirportCode as DepartureAirport, RealArrivalTime, RealDepartureTime, Frequency, Duration, @aircraftId as AircraftId, CarryOnPrice, CheckedPrice, TouristPrice, FirstClassPrice FROM @OutsideFlightRoutes as ofr
-    WHERE NOT EXISTS (
-        SELECT 1
-            FROM FlightRoute
-            WHERE arrivalAirport = ofr.ArrivalAirportCode
-            AND airlineId = ofr.AirlineId
-            AND departureAirport = ofr.DepartureAirportCode
-            AND frequency =ofr.Frequency
-            AND scheduledArrivalTime = ofr.RealArrivalTime
-            AND scheduledDepartureTime = ofr.RealDepartureTime
-            AND estimatedDuration = ofr.Duration
-            AND carryOnPrice = ofr.CarryOnPrice
-            AND checkedPrice = ofr.CheckedPrice
-            AND touristPrice = ofr.TouristPrice
-            AND firstClassPrice = ofr.FirstClassPrice
-    );
+        INSERT INTO dbo.FlightRoute(AdminId, AirlineId, ArrivalAirport, DepartureAirport, ScheduledArrivalTime, ScheduledDepartureTime, Frequency, EstimatedDuration, AircraftId, CarryOnPrice, CheckedPrice, TouristPrice, FirstClassPrice)
+        SELECT @adminId as AdminId, AirlineId, ArrivalAirportCode as ArrivalAirport, DepartureAirportCode as DepartureAirport, RealArrivalTime, RealDepartureTime, Frequency, Duration, @aircraftId as AircraftId, CarryOnPrice, CheckedPrice, TouristPrice, FirstClassPrice FROM @OutsideFlightRoutes as ofr
+        WHERE NOT EXISTS (
+            SELECT 1
+                FROM FlightRoute
+                WHERE arrivalAirport = ofr.ArrivalAirportCode
+                AND airlineId = ofr.AirlineId
+                AND departureAirport = ofr.DepartureAirportCode
+                AND frequency =ofr.Frequency
+                AND scheduledArrivalTime = ofr.RealArrivalTime
+                AND scheduledDepartureTime = ofr.RealDepartureTime
+                AND estimatedDuration = ofr.Duration
+                AND carryOnPrice = ofr.CarryOnPrice
+                AND checkedPrice = ofr.CheckedPrice
+                AND touristPrice = ofr.TouristPrice
+                AND firstClassPrice = ofr.FirstClassPrice
+        );
 
-	UPDATE fr
-    SET fr.Status = 'Habilitada'
-    FROM FlightRoute fr
-    INNER JOIN @OutsideFlightRoutes ofr
-        ON fr.AirlineId = ofr.AirlineId
-        AND fr.ArrivalAirport = ofr.ArrivalAirportCode
-        AND fr.DepartureAirport = ofr.DepartureAirportCode
-        AND fr.ScheduledArrivalTime = ofr.RealArrivalTime
-        AND fr.ScheduledDepartureTime = ofr.RealDepartureTime
-        AND fr.Frequency = ofr.Frequency
-        AND fr.EstimatedDuration = ofr.Duration
-    WHERE fr.AirlineId <> 1 AND fr.Status = 'Deshabilitada';
+	    UPDATE fr
+        SET fr.Status = 'Habilitada'
+        FROM FlightRoute fr
+        INNER JOIN @OutsideFlightRoutes ofr
+            ON fr.AirlineId = ofr.AirlineId
+            AND fr.ArrivalAirport = ofr.ArrivalAirportCode
+            AND fr.DepartureAirport = ofr.DepartureAirportCode
+            AND fr.ScheduledArrivalTime = ofr.RealArrivalTime
+            AND fr.ScheduledDepartureTime = ofr.RealDepartureTime
+            AND fr.Frequency = ofr.Frequency
+            AND fr.EstimatedDuration = ofr.Duration
+        WHERE fr.AirlineId <> 1 AND fr.Status = 'Deshabilitada';
+    COMMIT
 END
 GO
