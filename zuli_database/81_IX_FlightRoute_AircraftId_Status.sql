e2e050d76381ff39472bd7169570b5fb5f780916
randyrojasp <randy.rojas@ucr.ac.cr>
2026-07-02 00:54:33 -0600

    Actualizar el índice IX_FlightRoute_AircraftId_Status y simplificar la creación

--- zuli_database/81_IX_FlightRoute_AircraftId_Status.sql
+++ zuli_database/81_IX_FlightRoute_AircraftId_Status.sql
@@ -1,17 +1,5 @@
-USE ZuliAirlines;
+USE zuli;
 GO
-
-IF NOT EXISTS (
-    SELECT 1
-    FROM sys.indexes
-    WHERE object_id = OBJECT_ID('dbo.FlightRoute')
-      AND name = 'IX_FlightRoute_AircraftId_Status'
-)
-BEGIN
-    CREATE NONCLUSTERED INDEX IX_FlightRoute_AircraftId_Status
-    ON dbo.FlightRoute (
-        AircraftId,
-        Status
-    );
-END;
+CREATE NONCLUSTERED INDEX IX_FlightRoute_AircraftId_Status
+ON dbo.FlightRoute (AircraftId,Status);
 GO
